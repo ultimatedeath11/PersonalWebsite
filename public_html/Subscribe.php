8f38c25114e14cff09155f4ad02285f3d667ebbc
Noah Franck <ultimatedeath11@icloud.com>
2017-12-06 15:17:39 -0600

    folder form public

---
+++ public_html/Subscribe.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<?php $myfile = fopen("emailList.txt","a");
+$txt = $_POST["email"];
+fwrite($myfile,$txt);
+fwrite($myfile,"\n");
+fclose($myfile);
+ ?>
+
+
+Thank you for Subscribing<br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
